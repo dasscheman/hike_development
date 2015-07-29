acc48210e9c1c6dc55646106430a6ced87c690a8
dasscheman <d.asscheman@lumc.nl>
2015-07-29 14:58:45 +0200

    2015/07/29-14:58:44

--- hike_development/protected/tests/travis/tests/simpleTest.php
+++ hike_development/protected/tests/travis/tests/simpleTest.php
@@ -17,5 +17,26 @@ class admin_tests extends PHPUnit_Extensions_SeleniumTestCase
 	$this->assertEquals("Dat mag dus niet...", $this->getLocation());
         $this->assertTitle('phpunit selenium test');
     }
+
+    public function testLoadPageTwee()
+    {
+    	$this->open("http://127.0.0.1/index-test.php?r=site/login");
+        $this->waitForPageToLoad ( "30000" );
+	$this->assertEquals("HIKE-app", $this->getText("css=b"));
+    }
+
+    public function testLoadPageDrie()
+    {
+    	$this->open("http://127.0.0.1/hike_development/index-test.php?r=site/login");
+        $this->waitForPageToLoad ( "30000" );
+	$this->assertEquals("HIKE-app", $this->getText("css=b"));
+    }
+
+    public function testLoadPageVier()
+    {
+    	$this->open("http://127.0.0.1/hike_development/hike_development/index-test.php?r=site/login");
+        $this->waitForPageToLoad ( "30000" );
+	$this->assertEquals("HIKE-app", $this->getText("css=b"));
+    }
 }
 ?>
