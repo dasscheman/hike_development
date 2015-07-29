c481ec495e151ec0797184ad7880e39d61cafe79
dasscheman <d.asscheman@lumc.nl>
2015-07-29 16:01:33 +0200

    2015/07/29-16:01:32

--- hike_development/protected/tests/bootstrap.php
+++ hike_development/protected/tests/bootstrap.php
@@ -15,5 +15,5 @@ $config=dirname(__FILE__).'/../config/test.php';
 require_once($yiit);
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
-//Yii::$enableIncludePath = false;
+Yii::$enableIncludePath = false;
 Yii::createWebApplication($config);
