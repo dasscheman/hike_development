9298445ad9a1da95d7df204519ae632891f84e7f
dasscheman <d.asscheman@lumc.nl>
2015-07-29 14:46:18 +0200

    2015/07/29-14:46:16

--- hike_development/protected/tests/functional/ActionsGestartOrganisatieTest.php
+++ hike_development/protected/tests/functional/ActionsGestartOrganisatieTest.php
@@ -15,6 +15,21 @@ class ActionGestartOrganisatieTest extends WebTestCase
 	$this->assertEquals("Dat mag dus niet...", $this->getLocation());
         $this->assertTitle('phpunit selenium test');
     }
+    /*
+    protected function setUp()
+    {
+        $this->setBrowser('*firefox');
+        $this->setBrowserUrl('http://127.0.0.1/');
+        $this->shareSession(true);
+    }
+
+    public function testLoadPage()
+    {
+        $this->open('http://127.0.0.1/protected/tests/travis/simpleTest.html');
+        $this->waitForPageToLoad ( "30000" );
+	$this->assertEquals("Dat mag dus niet...", $this->getLocation());
+        $this->assertTitle('phpunit selenium test');
+    }
 
     public function testTitelControleren()
     {
