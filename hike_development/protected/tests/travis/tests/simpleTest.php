6aedb6ea4f5615e2aedd44f00edcb4be4eb93cc4
dasscheman <d.asscheman@lumc.nl>
2015-07-29 15:36:41 +0200

    2015/07/29-15:36:39

--- hike_development/protected/tests/travis/tests/simpleTest.php
+++ hike_development/protected/tests/travis/tests/simpleTest.php
@@ -20,35 +20,36 @@ class admin_tests extends PHPUnit_Extensions_SeleniumTestCase
 
     public function testLoadPageTwee()
     {
-    	$this->open("http://localhost/index-test.php");
+    	$this->open('http://localhost/index-test.php');
         $this->waitForPageToLoad ( "30000" );
-	$this->click("//ul[@id='yw2']/li/a/span/i[2]");
+	$this->assertTextPresent('HIKE-app');
 	$this->assertTrue((bool)preg_match('/^[\s\S]*index-test\.php[\s\S]r=site\/login$/',$this->getLocation()));
 	$this->assertEquals("HIKE-app", $this->getText("css=b"));
     }
 
     public function testLoadPageDrie()
     {
-    	$this->open("http://localhost/hike_development/index-test.php");
+    	$this->open('http://localhost/hike_development/index-test.php"');
         $this->waitForPageToLoad ( "30000" );
+	$this->assertTextPresent('HIKE-app');
 	$this->assertEquals("HIKE-app", $this->getText("css=b"));
     }
 
     public function testLoadPageVier()
     {
-    	$this->open("http://localhost/hike_development/hike_development/index-test.php");
+    	$this->open('http://localhost/hike_development/hike_development/index-test.php');
         $this->waitForPageToLoad ( "30000" );
+	$this->assertTextPresent('HIKE-app');
 	$this->click("//ul[@id='yw2']/li/a/span/i[2]");
 	$this->assertTrue((bool)preg_match('/^[\s\S]*index-test\.php[\s\S]r=site\/login$/',$this->getLocation()));
 	$this->assertEquals("HIKE-app", $this->getText("css=b"));
     }
 
-
-
-        public function testLoadPageVijf()
+    public function testLoadPageVijf()
     {
-    	$this->open("index-test.php");
+    	$this->open('index-test.php');
         $this->waitForPageToLoad ( "30000" );
+	$this->assertTextPresent('HIKE-app');
 	$this->click("//ul[@id='yw2']/li/a/span/i[2]");
 	$this->assertTrue((bool)preg_match('/^[\s\S]*index-test\.php[\s\S]r=site\/login$/',$this->getLocation()));
 	$this->assertEquals("HIKE-app", $this->getText("css=b"));
@@ -56,15 +57,17 @@ class admin_tests extends PHPUnit_Extensions_SeleniumTestCase
 
     public function testLoadPageZes()
     {
-    	$this->open("hike_development/index-test.php");
+    	$this->open('hike_development/index-test.php');
         $this->waitForPageToLoad ( "30000" );
+	$this->assertTextPresent('HIKE-app');
 	$this->assertEquals("HIKE-app", $this->getText("css=b"));
     }
 
     public function testLoadPageZeven()
     {
-    	$this->open("hike_development/hike_development/index-test.php");
+    	$this->open('hike_development/hike_development/index-test.php');
         $this->waitForPageToLoad ( "30000" );
+	$this->assertTextPresent('HIKE-app');
 	$this->click("//ul[@id='yw2']/li/a/span/i[2]");
 	$this->assertTrue((bool)preg_match('/^[\s\S]*index-test\.php[\s\S]r=site\/login$/',$this->getLocation()));
 	$this->assertEquals("HIKE-app", $this->getText("css=b"));
