d425d2c4548def5b31ec799cc11def029bdc682a
dasscheman <dasscheman@gmail.com>
2015-08-12 16:36:57 +0200

    12 augustus 2015

--- hike_development/protected/tests/functional/ActionsGestartOrganisatieTest.php
+++ hike_development/protected/tests/functional/ActionsGestartOrganisatieTest.php
@@ -370,7 +370,7 @@ class ActionGestartOrganisatieTest extends WebTestCase
 		$this->isElementPresent("link=Route Beheren");
 		$this->open("hike_development/index-test.php?r=route/index&event_id=3");
 
-
+		$this->assertEquals("2015-02-27", $this->getText("link=2015-02-27"));
         $this->assertContains("2015-02-25", $this->getBodyText());
 		$this->assertTrue($this->isElementPresent("//a[contains(@href, '#tab_3')]"));
 		$this->assertTrue($this->isElementPresent("link=2015-02-25"));
