2684a28c9774c14c80282f8ae7a0c4428d3b6fa0
dasscheman <dasscheman@gmail.com>
2015-08-23 12:43:33 +0200

    23 augustus 2015

--- hike_development/protected/tests/functional/ActionsGestartOrganisatieTest.php
+++ hike_development/protected/tests/functional/ActionsGestartOrganisatieTest.php
@@ -39,10 +39,12 @@ class ActionGestartOrganisatieTest extends WebTestCase
 		$this->check("id=LoginForm_rememberMe");
 		$this->click("name=yt0");
 		$this->waitForPageToLoad("30000");
+		$this->assertContains("hike_development/index-test.php", $this->getLocation());
+$this->assertContains("ASDFASDFASDF", $this->getBodyText());
+		$this->assertTrue(Yii::app()->user->name == "organisatie");
 		$this->assertEquals(1, Yii::app()->user->id);
 		$this->assertContains(1, Yii::app()->user->id);
 		$this->assertContains("testorganisatie", Yii::app()->user->id);
-		//$this->assertTrue(Yii::app()->user->name == "organisatie");
     }
 
     public function logout()
