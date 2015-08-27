8511ad144d0fa6a3b32fc4c35b86a65c3bd6e23a
dasscheman <dasscheman@gmail.com>
2015-08-27 23:09:21 +0200

    27 augustus 2015 google analytics is now only done when the site is on server with ip 87.195.107.103

--- hike_development/protected/views/layouts/main.php
+++ hike_development/protected/views/layouts/main.php
@@ -103,15 +103,18 @@
 	</div><!-- footer -->
 
 </div><!-- page -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-66849540-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
+<?php if ($_SERVER['SERVER_ADDR'] == "87.195.107.103") { ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-66849540-1', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
+<?php } ?>
 </body>
 </html>
