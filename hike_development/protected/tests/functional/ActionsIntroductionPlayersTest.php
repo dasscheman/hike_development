7365858445607460f89089870e10d5f333adf37f
dasscheman <d.asscheman@lumc.nl>
2015-08-28 15:09:59 +0200

    28 augustus 2015

--- hike_development/protected/tests/functional/ActionsIntroductionPlayersTest.php
+++ hike_development/protected/tests/functional/ActionsIntroductionPlayersTest.php
@@ -317,7 +317,6 @@ class ActionIntroductionPlayersTest extends WebTestCase
 		$this->open("hike_development/index-test.php?r=qrCheck/create&event_id=2&qr_code=55DlYLbS8Ws9EutrUMjNv6");
         $this->waitForPageToLoad ( "30000" );
 
-		$this->assertContains("DINGEN", $this->getBodyText());
 		$this->assertContains("/hike_development/index-test.php?r=qrCheck/viewPlayers&event_id=2&group_id=3", $this->getLocation());
 		$this->assertContains("introductie players", $this->getBodyText());
 
